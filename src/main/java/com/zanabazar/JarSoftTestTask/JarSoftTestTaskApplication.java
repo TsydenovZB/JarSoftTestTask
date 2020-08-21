1ff6edfd2bbd943dd1a10e44cabef839ddfb0920
zanabazar <tsydenov.zanabazar@gmail.com>
2020-08-21 17:16:17 +0700

    Init commit

---
+++ src/main/java/com/zanabazar/JarSoftTestTask/JarSoftTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.zanabazar.JarSoftTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JarSoftTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JarSoftTestTaskApplication.class, args);
+	}
+
+}
